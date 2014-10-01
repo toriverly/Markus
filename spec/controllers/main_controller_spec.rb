c242387e8d7657701b4b048c4582b69b72d9ec6f
toriverly <victoria.verlysdonk@gmail.com>
2014-09-30 18:00:49 -0600

    Changed a few describes to contexts, added a couple shared examples, implemented more examples

--- spec/controllers/main_controller_spec.rb
+++ spec/controllers/main_controller_spec.rb
@@ -13,15 +13,14 @@ describe MainController do
   # For AuthenticatedControllerTest stuff?
   # bypass cookie detection in the test because the command line, which is
   # running the test, cannot accept cookies
-  before(:each) do
-    request.cookies['cookieTest'] = 'fake cookie bypasses filter'
-  end
+  # before(:each) do
+  #   request.cookies['cookieTest'] = 'fake cookie bypasses filter'
+  # end
 
     context 'when a valid admin' do
-      let(:controller) { MainController.new }
       let(:admin) { build(:admin) }
-      let(:student) { build(:student) }
       let(:otherAdmin) { build(:admin) }
+      let(:student) { build(:student) }
       let(:otherStudent) { build(:student) }
 
       before :each do
@@ -29,66 +28,87 @@ describe MainController do
              :user_password => 'nonEmptyPassword'
       end
 
-      describe 'logs in' do
-        it 'has session uid set correctly' do
-          expect(admin.id).to eq session[:uid]
+      it 'has session uid set correctly' do
+        expect(session[:uid]).to eq admin.id
+      end
+
+      shared_examples 'failed role switch' do
+        let(:real_uid) { session[:real_uid] }
+
+        before do
+          post :login_as, :effective_user_login => effective_user_name,
+               :user_login => current_user.user_name,
+               :admin_password => password,
+               :uid => current_user.id, :timeout => 3.days.from_now
+        end
+
+        it 'does NOT switch role' do
+          expect(session[:uid]).to eq current_user.id
+          expect(session[:real_uid]).to eq real_uid
         end
       end
 
-      describe 'attempts to switch roles' do
+
+      shared_examples 'successful role switch' do
+        before do
+          post :login_as, :effective_user_login => effective_user.user_name,
+               :user_login => current_user.user_name,
+               :admin_password => 'nonEmptyPassword',
+               :uid => current_user.id, :timeout => 3.days.from_now
+        end
+
+        it 'switches role' do
+          expect(session[:uid]).to eq effective_user.id
+          expect(session[:real_uid]).to eq real_uid
+        end
+
+        it 'redirects to index'
+        # redirect_to :action => 'index'
+      end
+
+      context 'when attempting to switch roles' do
         context 'with empty data for effective user' do
-          it 'does not switch role' do
-            post :login_as, :effective_user_login => '',
-                 :user_login => admin.user_name,
-                 :admin_password => 'nonEmptyPassword',
-                 :uid => admin.id, :timeout => 3.days.from_now
-            expect(session[:real_uid]).to be_nil
+          include_examples 'failed role switch' do
+            let(:current_user) { admin }
+            let(:effective_user_name) { '' }
+            let(:password) { 'nonEmptyPassword' }
           end
         end
 
         context 'with empty data for password' do
-          it 'does not switch role' do
-            post :login_as, :effective_user_login => student.user_name,
-                 :user_login => admin.user_name,
-                 :admin_password => '''',
-                 :uid => admin.id, :timeout => 3.days.from_now
-            expect(session[:real_uid]).to be_nil
+          include_examples 'failed role switch' do
+            let(:current_user) { admin }
+            let(:effective_user_name) { student.user_name }
+            let(:password) { '' }
           end
         end
 
         context 'when effective user is another admin' do
-          it 'does not switch role' do
-            post :login_as, :effective_user_login => otherAdmin.user_name,
-                 :user_login => admin.user_name,
-                 :admin_password => 'nonEmptyPassword',
-                 :uid => admin.id, :timeout => 3.days.from_now
-            expect(session[:real_uid]).to be_nil
+          include_examples 'failed role switch' do
+            let(:current_user) { admin }
+            let(:effective_user_name) { otherAdmin.user_name }
+            let(:password) { 'nonEmptyPassword' }
           end
         end
 
         context 'when effective user is the same as current user' do
-          it 'does not switch role' do
-            post :login_as, :effective_user_login => admin.user_name,
-                 :user_login => admin.user_name,
-                 :admin_password => 'nonEmptyPassword',
-                 :uid => admin.id, :timeout => 3.days.from_now
-            expect(session[:real_uid]).to be_nil
+          include_examples 'failed role switch' do
+            let(:current_user) { admin }
+            let(:effective_user_name) { admin.user_name }
+            let(:password) { 'nonEmptyPassword' }
           end
         end
 
         context 'with valid data for a role with lesser privileges' do
-          it 'switches role' do
-            post :login_as, :effective_user_login => student.user_name,
-                 :user_login => admin.user_name,
-                 :admin_password => 'nonEmptyPassword',
-                 :uid => admin.id, :timeout => 3.days.from_now
-            expect(session[:uid]).to eq student.id
-            expect(session[:real_uid]).to eq admin.id
+          include_examples 'successful role switch' do
+            let(:current_user) { admin }
+            let(:effective_user) { student }
+            let(:real_uid) { admin.id }
           end
         end
       end
 
-      context 'when logged in as another user' do
+      context 'when role has been switched another user' do
         before :each do
           post :login_as,
                :effective_user_login => student.user_name,
@@ -97,31 +117,58 @@ describe MainController do
                :uid => admin.id, :timeout => 3.days.from_now
         end
 
-        # it 'renders the role_switch_handler template' do
-        #   expect(response).to render_template(:role_switch_handler)
-        # end
+        xit 'renders the role_switch_handler template' do
+          expect(response).to render_template(:role_switch_handler)
+        end
 
-        describe 'attempts to switch roles' do
+        context 'when attempting to switch roles' do
           context 'when effective user is another admin' do
-            it 'does not switch role'
+            include_examples('failed role switch') do
+              let(:current_user) { student }
+              let(:effective_user_name) { otherAdmin.user_name }
+              let(:password) { 'nonEmptyPassword' }
+            end
           end
 
           context 'when effective user is the same as current user' do
-            it 'does not switch role'
+            include_examples 'failed role switch' do
+              let(:current_user) { student }
+              let(:effective_user_name) { student.user_name }
+              let(:password) { 'nonEmptyPassword' }
+            end
           end
 
           context 'when effective user is the original user' do
-            it 'drops back to original role'
+            include_examples 'successful role switch' do
+              let(:current_user) { student }
+              let(:effective_user) { admin }
+              let(:real_uid) { nil }
+            end
           end
 
           context 'with valid data for a role with lesser privileges' do
-            it 'switches role'
+            include_examples 'successful role switch' do
+              let(:current_user) { student }
+              let(:effective_user) { otherStudent }
+              let(:real_uid) { admin.id }
+            end
           end
         end
 
-        context 'logs out' do
-          it 'redirects to login page'
-          it 'discards session info'
+        context 'when logged out' do
+          before :each do
+            # TODO: should this be student or admin?
+            get :logout, :uid => admin.id, :timeout => 3.days.from_now
+          end
+
+          xit 'redirects to login page' do
+            expect(controller).to redirect_to :action => 'login'
+          end
+
+          it 'discards session info' do
+            expect(session[:uid]).to be_nil
+            expect(session[:real_uid]).to be_nil
+          end
         end
       end
     end
